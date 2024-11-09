008686a147c2ca12757d59d9a593478d2664ae2d
Edwin Kimsal <kimsal.edwin@gmail.com>
2024-11-09 15:07:00 -0500

    Finished separate_datasets.py file

--- Research Prepare/separate_datasets.py
+++ Research Prepare/separate_datasets.py
@@ -1,6 +1,6 @@
 # Imports
+from PIL import Image
 import os
-
 
 # Node function
 def heapify(arr, n, i):
@@ -42,24 +42,30 @@ def heapSort(arr):
         heapify(arr, i, 0)
 
 
-# # Function to create datasets
-# def create_datasets(png_files, json_png_files):
-#     # Calculate the number of files for each dataset
-#     num_test = len(png_files) * per_test
-#     num_train = len(png_files) * per_train
-#     num_val = len(png_files) * per_val
-#
-#     # Create datasets
-#     test_set = png_files[:num_test]
-#     train_set = png_files[num_test:num_test + num_train]
-#     val_set = png_files[num_test + num_train:]
-#
-#     # Return datasets
-#     return test_set, train_set, val_set
+# Function to create datasets
+def create_datasets(input_dir, png_files, mask_files, png_output, mask_output):
+    # Print what it is working on
+    print(f"Working On: {png_output.split("\\")[-1]}")
+
+    # Save each file in png_files list
+    for png_file in png_files:
+        in_file = os.path.join(input_dir, png_file)
+        out_file = os.path.join(png_output, png_file)
+        with open(out_file, "w") as f:
+            im = Image.open(in_file)
+            im.save(out_file)
+
+    # Save each file in mask_files list
+    for mask_file in mask_files:
+        in_file = os.path.join(input_dir, mask_file)
+        out_file = os.path.join(mask_output, mask_file)
+        with open(out_file, "w") as f:
+            im = Image.open(in_file)
+            im.save(out_file)
 
 
 # Separate function
-def separtate(input_path):
+def separate(input_path):
     # Set list for .png and .json.png files
     png_files = []
     json_png_files = []
@@ -81,37 +87,56 @@ def main():
     # Percent vars (all must add up to one)
     per_test = .15
     per_train = .75
-    per_val = .10
-
-    # Dataset lists
-    test = []
-    test_mask = []
-    train = []
-    train_mask = []
-    val = []
-    val_mask = []
+    # per_val = 1 - (per_test + per_train)
 
     # Get directory
     script_directory = os.path.dirname(os.path.abspath(__file__))
 
+    # Set Dataset folder
+    dataset_folder = "Datasets"
+
     # Input and Output File
     input_file = "Output Split Images"
-    output_file = "Output Split Images"
+    test_file = "Test Set"
+    test_mask_file = "Test Mask Set"
+    train_file = "Train Set"
+    train_mask_file = "Train Mask Set"
+    val_file = "Val Set"
+    val_mask_file = "Val Mask Set"
 
     # Set file_paths
     input_path = os.path.join(script_directory, input_file)
-    output_path = os.path.join(script_directory, output_file)
+    test_path = os.path.join(script_directory, dataset_folder, test_file)
+    test_mask_path = os.path.join(script_directory, dataset_folder, test_mask_file)
+    train_path = os.path.join(script_directory, dataset_folder, train_file)
+    train_mask_path = os.path.join(script_directory, dataset_folder, train_mask_file)
+    val_path = os.path.join(script_directory, dataset_folder, val_file)
+    val_mask_path = os.path.join(script_directory, dataset_folder, val_mask_file)
 
     # Call separate function
-    png_files, json_png_files = separtate(input_path)
+    png_files, json_png_files = separate(input_path)
 
     # Order the lists
     heapSort(png_files)
     heapSort(json_png_files)
 
-    # Print each list
-    print(png_files)
-    print(json_png_files)
+    # Get amount of files in each category
+    num_test = int(len(png_files) * per_test)
+    num_train = int(len(png_files) * per_train)
+    num_val = len(png_files) - (num_test + num_train)
+
+    # Create dataset lists
+    test = png_files[:num_test]
+    test_mask = json_png_files[:num_test]
+    train = png_files[num_test:num_test + num_train]
+    train_mask = json_png_files[num_test:num_test + num_train]
+    val = png_files[num_test + num_train:]
+    val_mask = json_png_files[num_val:]
+
+    # Create dataset folders
+    create_datasets(input_path, test, test_mask, test_path, test_mask_path)
+    create_datasets(input_path, train, train_mask, train_path, train_mask_path)
+    create_datasets(input_path, val, val_mask, val_path, val_mask_path)
 
 
 # Call main function
