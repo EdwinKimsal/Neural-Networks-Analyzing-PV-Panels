8a203ff572b875bfad23973bb9dcb612b639c929
Edwin Kimsal <kimsal.edwin@gmail.com>
2024-11-14 13:59:52 -0500

    Worked on removing non-PV images

--- Research Prepare/img_remover.py
+++ Research Prepare/img_remover.py
@@ -1,27 +1,26 @@
 # Imports
 import os
-# import cv2
+import cv2
 
 # Function to remove images
-def remove(input_path, file):
-    # Remove file
-    os.remove(os.path.join(input_path, file))
+def add(input_path, output_path, file_mask, file_img):
+    # Add file
+    os.save(os.path.join(output_path, file_mask))
     # Print file removed
-    print("Removed: " + file)
-
-    # Remove .json from file
-    file = file.replace(".json", "")
+    print("Added: " + file_mask)
 
     # Remove file
-    os.remove(os.path.join(input_path, file))
+    os.save(os.path.join(output_path, file_img))
+
     # Print file removed
-    print("Removed: " + file)
+    print("Added: " + file_img)
+
 
 # Main function
 def main():
     # Path to Split Images, Output Images, and cmd
     input = "Output Split Images"
-    output = "Output Split Images"
+    output = "Final Output Split Images"
     cmd = os.getcwd()
 
     # Set paths
@@ -29,22 +28,26 @@ def main():
     output_path = os.path.join(cmd, output)
 
     # Set blank json list
-    json_list = []
+    mask_list = []
+
+    # Set blank image list
+    img_list = []
 
     # Iterate through all the files in Split Images
     for file in os.listdir(input_path):
         # If .json.png file, append to json_list
-        if file.endswith(".json.png"):
-            json_list.append(file)
+        if "_mask_" in file:
+            mask_list.append(file)
+
+        else:
+            img_list.append(file)
 
     # Iterate through all the files in json_list
-    for file in json_list:
+    for i in range(len(mask_list)):
         # If all pixels in file are black, remove file
-        if cv2.imread(os.path.join(input_path, file)).all() == 0:
-            remove(input_path, file)
+        if cv2.imread(os.path.join(input_path, mask_list[i])).all() != 0:
+            add(input_path, output_path, mask_list[i], img_list[i])
 
-        # Print file
-        print(os.path.join(input_path, file))
 
 # Call main function
 main()
