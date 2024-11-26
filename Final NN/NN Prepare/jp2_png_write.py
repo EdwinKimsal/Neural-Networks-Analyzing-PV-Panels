b67ac006d8dcd54ce627feb38f0ed7646a79e376
Edwin Kimsal <kimsal.edwin@gmail.com>
2024-11-25 20:16:52 -0500

    Created jp2_png_write script to put all full jp2_png files into a txt file that can be used to save progress of splitting jp2_png files

---
+++ Final NN/NN Prepare/jp2_png_write.py
@@ -0,0 +1,26 @@
+# Import(s)
+import os
+
+# Main Function
+def main():
+    # Set file names
+    in_file = os.path.join(".", "JP2000_PNG Files")
+    out_file = os.path.join(".", "NY-Q", "tiles", "jp2_list.txt")
+
+    # Create a list of all files in "JP2000_PNG Files"
+    files = os.listdir(in_file)
+
+    # Write starting index
+    with open(out_file, "w") as f:
+        f.write("0\n")
+
+        # Write each file to jp2_list.txt
+        for file in files:
+            f.write(f"{file}\n")
+
+
+# Call main function
+main()
+
+# Signify end of program
+print("\nEnd of Program")
