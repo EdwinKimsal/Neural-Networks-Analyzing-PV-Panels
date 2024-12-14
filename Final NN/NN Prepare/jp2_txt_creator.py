60e2e91643bb4827902f77cce02d06d73a30906f
Edwin Kimsal <kimsal.edwin@gmail.com>
2024-12-13 19:15:26 -0500

    Added file to write all jp2 files in a single txt file for convertgin jp2 files to png files

--- Final NN/NN Prepare/jp2_txt_creator.py
+++ Final NN/NN Prepare/jp2_txt_creator.py
@@ -1,2 +1,35 @@
 # Import(s)
 import os
+
+# add_file function
+def add_file(file, txt_file):
+    with open(txt_file, "a") as f:
+        f.write(f"{file}\n")
+
+
+# Main function
+def main():
+    # Files paths
+    cwd = os.getcwd()
+    input_files = os.path.join(cwd, "Four Channel ZIP Files")
+    output_txt_file = os.path.join(cwd, "NY-Q", "tiles", "jp2_all.txt")
+
+    # Reset output_txt_file
+    with open(output_txt_file, "w") as f:
+        pass
+
+    # Set file_list to blank
+    file_list = []
+
+    # Iterate through file_path
+    for (root, dirs, file) in os.walk(input_files):
+
+        # Iterate through each file
+        for f in file:
+            # If the file ends with .jp2 add it to output txt file
+            if f.endswith(".jp2") == True:
+                print(f)
+                add_file(f, output_txt_file)
+
+# Call main function
+main()
