2dc9a9a31262ebca28cd3e1e1947b16b72f4c35b
Edwin Kimsal <kimsal.edwin@gmail.com>
2024-12-16 17:55:41 -0500

    Removed global variables

--- Final NN/NN Prepare/img_divider.py
+++ Final NN/NN Prepare/img_divider.py
@@ -10,33 +10,17 @@ as a new image.
 from PIL import Image
 import os
 
-# Customizable (global) vars
-original_sub_img_side = 625  # original_sub_img_side must be equally divisible by this var)
-input_txt = "jp2_list.txt"
-input_file = "JP2000_PNG Files"
-output_file = "jp2"
-
-# Get this file's folder's directory
-cwd = os.getcwd()
-
-# Set file_paths
-input_txt_path = os.path.join(cwd, "NY-Q", "tiles", input_txt)
-input_file_path = os.path.join(cwd, input_file)
-
-# Final file type
-final_file_type = "png"
-
 # write_file Function
-def write_file(file_list, img_per_row, final_sub_img_side):
+def write_file(file_list, img_per_row, final_sub_img_side, input_txt_path, final_file_type, input_file_path, original_sub_img_side, cwd, output_file):
     with open(input_txt_path, "w") as f:
         for line in file_list:
             f.write(f"{line}\n")
 
-    return img_divider(file_list, img_per_row, final_sub_img_side)
+    return img_divider(file_list, img_per_row, final_sub_img_side, input_txt_path, final_file_type, input_file_path, original_sub_img_side, cwd, output_file)
 
 
 # img_divider Function
-def img_divider(file_list, img_per_row, final_sub_img_side):
+def img_divider(file_list, img_per_row, final_sub_img_side, input_txt_path, final_file_type, input_file_path, original_sub_img_side, cwd, output_file):
     # Base case end
     if len(file_list) == 0:
         return file_list
@@ -75,11 +59,11 @@ def img_divider(file_list, img_per_row, final_sub_img_side):
                 im.save(os.path.join(cwd, output_file, f"{file.replace(".png", "")}_{file_name}"))
 
     # Recall function removing first ele
-    return write_file(file_list[1:], img_per_row, final_sub_img_side)
+    return write_file(file_list[1:], img_per_row, final_sub_img_side, input_txt_path, final_file_type, input_file_path, original_sub_img_side, cwd, output_file)
 
 
 # calculate_vars Function
-def calc_vars(file_list, img_side):
+def calc_vars(file_list, img_side, original_sub_img_side, input_txt_path, final_file_type, input_file_path, cwd, output_file):
     # Calculate imgs_per_row
     img_per_row = img_side // original_sub_img_side
 
@@ -87,11 +71,27 @@ def calc_vars(file_list, img_side):
     final_sub_img_side = original_sub_img_side
 
     # Call img_divider Function
-    return img_divider(file_list, img_per_row, final_sub_img_side)
+    return img_divider(file_list, img_per_row, final_sub_img_side, input_txt_path, final_file_type, input_file_path, original_sub_img_side, cwd, output_file)
 
 
 # Start function
 def main():
+    # Customizable (global) vars
+    original_sub_img_side = 625  # original_sub_img_side must be equally divisible by this var)
+    input_txt = "jp2_list.txt"
+    input_file = "JP2000_PNG Files"
+    output_file = "jp2"
+
+    # Get this file's folder's directory
+    cwd = os.getcwd()
+
+    # Set file_paths
+    input_txt_path = os.path.join(cwd, "NY-Q", "tiles", input_txt)
+    input_file_path = os.path.join(cwd, input_file)
+
+    # Final file type
+    final_file_type = "png"
+
     # Create empty file_list
     file_list = []
 
@@ -113,7 +113,7 @@ def main():
         break
 
     # Call calc_vars
-    file_list = calc_vars(file_list, img_side)
+    file_list = calc_vars(file_list, img_side, original_sub_img_side, input_txt_path, final_file_type, input_file_path, cwd, output_file)
 
     # Call Write File
     write_file(file_list)
